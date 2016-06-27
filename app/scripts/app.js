30257419f80969ef06bd216eddf7aa59c7e54f19
gerardogrimaldi <gerardo.grimaldi@gmail.com>
2016-06-27 12:59:15 -0300

    first

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name homeAppApp
+ * @description
+ * # homeAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('homeAppApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name homeAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the homeAppApp
+ */
+angular.module('homeAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
